22/01/2024: fixed code for temp scaling

streamlit/interactive_city_recommender.py
@@ -121,10 +121,10 @@ def rank_eval(all_data_df, train_df_pca, wht_dict):
     scaled_best_std_dev_tmp = (wht_dict['wt_std_dev'] - all_data_df['std_dev_temp'].min()) / std_dev_temp_range
 
     # Normalize the new point
-    scaled_best_mean_tmp = ( (scaled_best_mean_tmp - final_df['scaled_mean_tmp'].mean()) /
-                                           final_df['scaled_mean_tmp'].std() )
-    sclaed_best_std_dev_tmp = ( (sclaed_best_std_dev_tmp - final_df['scaled_tmp_std_dev'].mean()) /
-                                           final_df['scaled_tmp_std_dev'].std() )
+    scaled_best_mean_tmp = ( (scaled_best_mean_tmp - all_data_df['scaled_mean_tmp'].mean()) /
+                                           all_data_df['scaled_mean_tmp'].std() )
+    sclaed_best_std_dev_tmp = ( (sclaed_best_std_dev_tmp - all_data_df['scaled_tmp_std_dev'].mean()) /
+                                           all_data_df['scaled_tmp_std_dev'].std() )
 
     new_point = np.array([[wht_dict['wt_cost_living'],wht_dict['wt_purchase_pow'],
         wht_dict['wt_safety'],wht_dict['wt_pollution'],wht_dict['wt_traffic'],